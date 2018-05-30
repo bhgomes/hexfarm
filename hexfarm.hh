cf9a478688cdd8be282c67bba35881249c73909f
bhgomes <bhgomes.github@gmail.com>
2018-05-29 20:39:57 -0400

    add helper file

---
+++ hexfarm.hh
@@ -0,0 +1,14 @@
+#ifndef HEXFARM__HEXFARM_HH
+#define HEXFARM__HEXFARM_HH
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+
+namespace hexfarm {
+
+
+
+} /* namespace hexfarm */
+
+#endif /* HEXFARM__HEXFARM_HH */
