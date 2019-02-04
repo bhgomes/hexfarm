da47f2504e9c97ce328ac9b1098fe9feb8b27658
bhgomes <bhgomes.github@gmail.com>
2019-02-04 13:57:14 -0500

    update script

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,38 +1,17 @@
 #!/bin/sh
+# Hexfarm Setup Script
+# (bhgomes)
 
 cd $HOME
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
-mv .linuxbrew cmslink/.linuxbrew
-mv .cache cmslink/.cache
-
-# this should already be in your bashrc
-export PATH="$HOME/cmslink/.linuxbrew/bin:$HOME/cmslink/.linuxbrew/sbin:$PATH"
-
-brew vendor-install ruby
-
-HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_BUILD_FROM_SOURCE=1 brew install gcc --without-glibc
-HOMEBREW_NO_AUTO_UPDATE=1 brew install glibc
-HOMEBREW_NO_AUTO_UPDATE=1 brew remove gcc
-HOMEBREW_NO_AUTO_UPDATE=1 brew install gcc
-
-brew upgrade
-
-brew install gcc@7
-brew install cmake
-brew install python3
-brew install htop tree pipenv ipython node
-
-cp /users/h2/bhgomes/extern/Miniconda3-latest-Linux-x86_64.sh $HOME/
-
-# when it asks you if you want to change the path type:
-#    /users/h2/ag1378/cmslink/.miniconda3
 bash Miniconda3-latest-Linux-x86_64.sh
-
 rm Miniconda3-latest-Linux-x86_64.sh
 
 conda upgrade conda
-conda install anaconda conda-build
-conda config --add channels conda-forge bhgomes
-conda install root geant4 pythia8
+conda install pip, root
+
+pip install hexfarm
+
+conda clean --all
